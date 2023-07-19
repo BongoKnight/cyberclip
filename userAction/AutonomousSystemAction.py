3969a3b17741927d44883896e38f0e09d51e294b
SVernin <simon.vernin@gmail.com>
2023-07-19 17:19:25 +0200

    Fix Ipv6 parser and add IPv6 to AS action

--- userAction/AutonomousSystemAction.py
+++ userAction/AutonomousSystemAction.py
@@ -13,6 +13,7 @@ except:
     from actionInterface import actionInterface
 
 AS_DB = pd.DataFrame()
+AS_IPv6_DB = pd.DataFrame()
 AS_RANGE = pd.DataFrame()
 
 def get_AS_data():
@@ -37,7 +38,32 @@ def get_AS_data():
         print("Error while loading AS database")
     return AS_DB
 
+def get_IPv6_AS_data():
+    global AS_IPv6_DB
+    file_name = os.path.join("as_ipv6.gz")
+    if not os.path.exists(file_name) or (time.time() - os.path.getmtime(file_name) ) / 3600 > 24*7:
+        try:
+            url = "https://iptoasn.com/data/ip2asn-v6.tsv.gz"
+            response = requests.get(url, stream=True)
+            with open(file_name, 'wb') as f:
+                shutil.copyfileobj(response.raw,f)
+            del response
+        except Exception as e:
+            print("Error while retriving database from IpToASN")
+    try: 
+        AS_IPv6_DB = pd.read_csv(file_name, header=None, sep="\t", compression="gzip")
+        AS_IPv6_DB.rename(columns={0:"min_ip",1:"max_ip",2:"as_num",3:"as_loc",4:"as_info"}, inplace=True)
+        AS_IPv6_DB["min_ip"] =  AS_IPv6_DB["min_ip"].apply(ipaddress.ip_address)
+        AS_IPv6_DB["max_ip"] =  AS_IPv6_DB["max_ip"].apply(ipaddress.ip_address)
+        AS_IPv6_DB["CIDRS"] = AS_IPv6_DB.apply(lambda row: [i for i in ipaddress.summarize_address_range(
+            row["min_ip"],
+            row["max_ip"])], axis=1)
+    except Exception as e:
+        print("Error while loading AS database")
+    return AS_IPv6_DB
+
 get_AS_data()
+get_IPv6_AS_data()
 
 class AsToCidrAction(actionInterface):
     """
@@ -85,7 +111,7 @@ class IpToAsAction(actionInterface):
     Return Autonomous System information refering to an IP.
     """
     
-    def __init__(self, parsers = {}, supportedType = {"ip"}):
+    def __init__(self, parsers = {}, supportedType = {"ipv6","ip"}):
         super().__init__(parsers = parsers, supportedType = supportedType)
         self.description = "IP to AS Number"
 
@@ -104,11 +130,19 @@ class IpToAsAction(actionInterface):
                                 "max_ip": str(ipaddress.ip_address(infos.get("max_ip",0))),
                                 "as_num":"AS" + str(infos.get("as_num"))})
                         self.results.update({ip:infos})
-                return self.results
-            else:
-                return self.observables.get("ip", [])
-        else:
-            return []
+        if self.observables.get("ipv6", []):
+            if len(AS_IPv6_DB.columns):
+                ips = self.observables.get("ipv6", [])
+                for ip in ips:
+                    ip_object = ipaddress.ip_address(ip)
+                    response = AS_IPv6_DB.loc[(AS_IPv6_DB["min_ip"]<ip_object) & (AS_IPv6_DB["max_ip"]>=ip_object)].to_dict(orient="row")
+                    if len(response)>0:
+                        infos = response[0]
+                        infos.update({"min_ip": str(infos.get("min_ip",0)),
+                                "max_ip": str(infos.get("max_ip",0)),
+                                "as_num":"AS" + str(infos.get("as_num"))})
+                        self.results.update({ip:infos})
+        return self.results
 
     def __str__(self):
         """Visual representation of the action"""
