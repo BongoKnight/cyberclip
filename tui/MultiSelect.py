99c40598d20220ad69ffe5021a7d1dd3a1eb0536
SVernin <simon.vernin@gmail.com>
2023-10-03 16:23:43 +0200

    Multi select tupple entry

--- tui/MultiSelect.py
+++ tui/MultiSelect.py
@@ -1,4 +1,5 @@
 from textual.widgets import Static, Button, Input, Label, Select
+from textual.widgets import _select
 from textual.containers import Horizontal, HorizontalScroll
 from textual.app import ComposeResult
 from textual.reactive import reactive, var
@@ -19,16 +20,22 @@ class MultiSelect(Static):
         margin-top: 1;
     }
     """
-    def __init__(self, label : str ="Select options", value : set = set(), options : list = [], **kwargs):
+    def __init__(self, label : str ="", value : set = set(), options : list = [], **kwargs):
         super().__init__(**kwargs)
         self._value = set(value)
         self.label = label
-        self.options = [(f"{FALSE_EMOTE} {option}", option) for option in options]
+        self.options = []
+        if options:
+            if isinstance(options[0], str) :
+                self.options = [(f"{FALSE_EMOTE} {option}", option) for option in options]
+            elif isinstance(options[0], tuple):
+                self.options = [(f"{TRUE_EMOTE} {option}", option) if is_active else (f"{FALSE_EMOTE} {option}", option) for option, is_active in options]
+        self.options.sort(key=lambda option: option[1])
     
     def compose(self) -> ComposeResult:
         yield Horizontal(
             Label(renderable=self.label, id="title"),
-            Select(self.options,prompt=self.label ,classes="multiselect")
+            Select(self.options,prompt="Select options..." ,classes="multiselect")
         )
     
 
@@ -42,10 +49,15 @@ class MultiSelect(Static):
                 else :
                     new_options[index] = (f"{FALSE_EMOTE} " + option[1], option[1])
                 self.options = new_options
-                event.select.set_options(new_options)
-                event.select.value = None
+                event.control.set_options(new_options)
+                event.control.value = None
                 break
-
+    
+    @on(_select.SelectOverlay.UpdateSelection)
+    def reopen(self, event: _select.SelectOverlay.UpdateSelection):
+        self.app.notify("Trying to give focus")
+        
+    
     @property
     def value(self) -> list[str]:
         return [option[1] for option in self.options if option[0].startswith(TRUE_EMOTE)]
@@ -65,13 +77,14 @@ if __name__ == "__main__":
         def compose(self) -> ComposeResult:
 
             yield Vertical(
-                MultiSelect(options=["Leto","Jessica","Paul"]),
+                MultiSelect(options=["Arrakis","Chapterhouse","Caladan"]),
+                MultiSelect(options=[("Leto",True),("Jessica", False),("Paul",True)]),
                 Log(id="log")
             )
         
         @on(Select.Changed)
         def update(self, event: Select.Changed):
-            values = self.query_one(MultiSelect).value
+            values = event.select.parent.parent.value
             self.app.query_one(Log).write_line(str(values))
     app = SelectApp()
     app.run()
