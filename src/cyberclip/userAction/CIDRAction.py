b8db999da19bef1fbbb1f38bb71be3f3612d488f
Bongoknight <>
2023-12-28 16:26:42 +0100

    CIDR/24 for IP

--- src/cyberclip/userAction/CIDRAction.py
+++ src/cyberclip/userAction/CIDRAction.py
@@ -20,7 +20,7 @@ class ToCIDR24Action(actionInterface):
     """
     A action module, to return a list of /24 CIDR from a bigger CIDR.
     """
-    def __init__(self, parsers = {}, supportedType = {"cidr"}):
+    def __init__(self, parsers = {}, supportedType = {"cidr", "ip"}):
         super().__init__(parsers = parsers, supportedType = supportedType)
         self.description = "To CIDR/24"
         self.exploded_cidr = {}
@@ -29,8 +29,11 @@ class ToCIDR24Action(actionInterface):
         """Execute the action."""
         self.exploded_cidr = {}
         self.observables = self.get_observables()
-        for cidr in self.observables.get("cidr"):
+        for cidr in self.observables.get("cidr", []):
             self.exploded_cidr.update({cidr:", ".join(explode_to(cidr))})
+        for ip in self.observables.get("ip", []):
+            network = ".".join(ip.split(".")[:-1]) + ".0/24"
+            self.exploded_cidr.update({ip:network})
         return self.exploded_cidr
     
     def __str__(self):
