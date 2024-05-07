772efd014e2188337f2a939633d86dc31ed06046
SVernin <>
2024-05-07 11:58:30 +0200

    Merge prepend/append action in one file

--- src/cyberclip/userAction/prependAction.py
+++ src/cyberclip/userAction/prependAction.py
@@ -3,6 +3,31 @@ try:
 except:
     from actionInterface import actionInterface
 
+
+class appendEachLineAction(actionInterface):   
+    """A action module, to append data to each line of a given text.  
+    Add the text to add as a param.
+    """ 
+    def __init__(self, parsers = {}, supportedType = {"text"}, complex_param: dict = {"Text to add":{"type":"text","value":""}}):
+        super().__init__(parsers = parsers, supportedType = supportedType, complex_param= complex_param)
+        self.description = "Append text on each line."
+
+    def execute(self) -> object:
+        """Add a line of text at the beginning of each line of a text.
+        
+        Return:
+            text (str): Text with prepended data
+        """
+        self.observables = self.get_observables()
+        if self.observables.get("text"):
+            text = self.observables.get("text")[0].splitlines()
+            return "\r\n".join([line + self.get_param_value("Text to add").replace("\\t","\t") for line in text])
+
+    
+    def __str__(self):
+        """Add a line of text at the beginning of a text."""
+        return  self.execute()
+
 class prependAction(actionInterface):   
     """A action module, to prepend data to a given text.
     
@@ -29,6 +54,31 @@ class prependAction(actionInterface):
     def __str__(self):
         """Add a line of text at the beginning of a text."""
         return  self.execute()
+
+
+class prependEachLineAction(actionInterface):   
+    """A action module, to prepend data to all the lines of a given text.
+    Enter the text to preprend as a param.
+    """ 
+
+    def __init__(self, parsers = {}, supportedType = {"text"}, complex_param: dict={"Text to prepend":{"type":"text","value":""}}):
+        super().__init__(parsers = parsers, supportedType = supportedType, complex_param = complex_param)
+        self.description = "Prepend text on each line."
+        
+    def execute(self) -> object:
+        """Add a line of text at the end of each line of a text.
+        
+        Return:
+            text (str): Text with prepended data
+        """
+        self.observables = self.get_observables()
+        text = self.observables.get("text","")[0].splitlines()
+        return "\r\n".join([self.get_param_value("Text to prepend").replace("\\t","\t") + line for line in text])
+
+
+    def __str__(self):
+        return  self.execute()
+
 
 if __name__=='__main__':
     from userTypeParser.TextParser import TextParser
