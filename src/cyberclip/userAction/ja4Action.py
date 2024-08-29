8e0b18c41478aea9529afc1419749dce71cee1ba
SVernin <>
2024-08-29 16:49:59 +0200

    🆕JA3 and JA4 fingerprints lookup on online databases

---
+++ src/cyberclip/userAction/ja4Action.py
@@ -0,0 +1,119 @@
+import pandas as pd
+from pathlib import Path
+import requests
+import os
+import json
+import time
+try:
+    from userAction.actionInterface import actionInterface
+except:
+    from actionInterface import actionInterface
+
+JA4_DB = None
+JA3_DB = None
+
+
+def get_JAX_data():
+    global JA4_DB, JA3_DB
+    JA4_DB = pd.DataFrame()
+    file_name = os.path.join(Path(__file__).parent / "../data/ja4.json")
+    if not os.path.exists(file_name) or (time.time() - os.path.getmtime(file_name) ) / 3600 > 24*7:
+        try:
+            url = "https://ja4db.com/api/download/"
+            response = requests.get(url)
+            with open(file_name, 'w') as f:
+                json.dump(response.json(),f)
+            del response
+        except Exception as e:
+            print("Error while retriving JA4+ database from ja4db.com", e)
+
+    try: 
+        JA4_DB = pd.read_json(file_name)
+    except Exception as e:
+        print("Error while loading JA4 database", e)
+
+
+    JA3_DB = pd.DataFrame()
+    file_name = os.path.join(Path(__file__).parent / "../data/ja3.json")
+    if not os.path.exists(file_name) or (time.time() - os.path.getmtime(file_name) ) / 3600 > 24*7:
+        try:
+            url = "https://ja3.me/download/ja3me.json"
+            response = requests.get(url)
+            with open(file_name, 'w') as f:
+                json.dump(response.json(),f)
+            del response
+        except Exception as e:
+            print("Error while retriving JA3 database from ja3.me", e)
+
+    try: 
+        JA3_DB = pd.read_json(file_name)
+    except Exception as e:
+        print("Error while loading JA3 database", e)
+
+
+class JA4Action(actionInterface):
+    """A action module, to search for JA4+.  
+    Return the software or librairy associated to a JA4 fingerprint.
+    """
+
+    def __init__(self, parsers = {}, supportedType = {"ja4"}):
+        super().__init__(parsers = parsers, supportedType = supportedType)
+        self.description = "JA4: Search"
+        
+    def execute(self) -> object:
+        """
+        Return JA4+ data. Data returned include: Matching_Values, application, library, device, os, user_agent_string, certificate_authority, verified, notes.
+        """
+        get_JAX_data()
+        self.observables = self.get_observables()
+        ja4s = self.observables.get("ja4")
+        if ja4s:
+            mask = JA4_DB.isin(ja4s)
+            matching_df = JA4_DB
+            matching_df['Matching_Values'] = matching_df.where(mask).bfill(axis=1).iloc[:, 0]
+            filtered_df = matching_df[matching_df['Matching_Values'].notna()]
+            return filtered_df
+        else:
+            return pd.DataFrame(columns=["Matching_Values","application", "library", "device", "os", "user_agent_string", "certificate_authority", "verified", "notes"])
+    
+    def __str__(self):
+        """Returns a TSV representation of the dataframe returned by `execute`"""
+        df = self.execute()
+        return  df[["Matching_Values","application", "library", "device", "os", "user_agent_string", "certificate_authority", "verified", "notes"]].to_csv(sep="\t", index=None)
+
+class JA3Action(actionInterface):
+    """A action module, to search for JA3.  
+    Return the software or librairy associated to a JA3 fingerprint.
+    """
+
+    def __init__(self, parsers = {}, supportedType = {"md5"}):
+        super().__init__(parsers = parsers, supportedType = supportedType)
+        self.description = "JA3: Search"
+        
+    def execute(self) -> object:
+        """
+        Return JA3 data. Data returned include: digest, first_seen, last_seen, user_agent, ja3, source.
+        """
+        get_JAX_data()
+        self.observables = self.get_observables()
+        ja3s = self.observables.get("md5")
+        if ja3s:
+            return JA3_DB[JA3_DB['digest'].isin(ja3s)]
+        else:
+            return pd.DataFrame(columns=["digest","first_seen","last_seen","user_agent","ja3","source"])
+    
+    def __str__(self):
+        """Returns a TSV representation of the dataframe returned by `execute`"""
+        df = self.execute()
+        return  df[["digest","first_seen","last_seen","user_agent","ja3","source"]].to_csv(sep="\t", index=None)
+
+
+if __name__=='__main__':
+    from userTypeParser.JA4Parser import ja4Parser
+    from userTypeParser.MD5Parser import md5Parser
+    data = "ip\tt13d301200_1d37bd780c83_d339722ba4af info\n154.0.123.1\tT1548 0b2012eb6e9a1f76ba56587d4a391211"
+    ja4_parser = ja4Parser(data)
+    ja3_parser = md5Parser(data)
+    a = str(JA4Action({"ja4":ja4_parser},["ja4"]))
+    print(a, ja4_parser.objects)
+    print(str(JA3Action({"md5":ja3_parser},["md5"])), ja3_parser.objects)
---
+++ src/cyberclip/userTypeParser/JA4Parser.py
@@ -0,0 +1,56 @@
+import re
+try:
+    from userTypeParser.ParserInterface import ParserInterface
+except:
+    from ParserInterface import ParserInterface
+
+class ja4Parser(ParserInterface):
+    """Implementation of ParserInterface for JA4 hash.
+
+    Code exemple ::
+        a = ja4Parser("1.3.4.5")
+        b = ja4Parser("ge11cn020000_9ed1ff1f7b03_cd8dafe26982")
+        print(a.extract(), a.contains())
+        print(b.extract(), b.contains())
+
+    """
+    
+    def __init__(self, text: str, parsertype="ja4"):
+        self.text = text
+        self.parsertype = "ja4"
+        self.objects = []
+        
+    def contains(self):
+        """Return true if text contains at least one md5."""
+        if re.search(r"\b(JA4(H|X|SSH|S|T|TScan)?=?)?([a-z0-9]{4,20}_[a-z0-9]{4,20}(_[a-z0-9]{4,20})?|(\d+)_\d+-[\d\-_]+)\b",self.text) :
+            return True
+        else :
+            return False
+    
+    def extract(self):
+        """Return all md5 contained in text."""
+        ja4sIter = re.finditer(r"\b(JA4(H|X|SSH|S|T|TScan)?=?)?([a-z0-9]{4,20}_[a-z0-9]{4,20}(_[a-z0-9]{4,20})?|(\d+)_\d+-[\d\-_]+)\b", self.text)
+        ja4s = [ja4.group(3) for ja4 in ja4sIter]
+        self.objects = ja4s
+        return ja4s
+        
+        
+if __name__=="__main__":
+    a = ja4Parser("1.3.4.5")
+    b = ja4Parser("""JA4=t13d1516h2_8daaf6152771_02713d6af862	JA4=q13d0312h3_55b375c5d22e_06cda9e17597
+        JA4H=ge11cn020000_9ed1ff1f7b03_cd8dafe26982	
+        JA4=t13d201100_2b729b4bf6f3_9e7b989ebec8	JA4S=t120300_c030_5e2616a54c73
+        JA4=t13d190900_9dc949149365_97f8aa674fd9	JA4S=t130200_1301_a56c5b993250
+        JA4H=ge11cn060000_4e59edc1297a_4da5efaf0cbd	JA4X=2166164053c1_2166164053c1_30d204a01551
+        JA4=t13d880900_fcb5b95cb75a_b0d3b4ac2a14	JA4S=t130200_1302_a56c5b993250
+        JA4X=2bab15409345_af684594efb4_000000000000	
+        JA4X=1a59268f55e5_1a59268f55e5_795797892f9c	
+        JA4H=po10nn060000_cdb958d032b0	
+        JA4H=po11nn050000_d253db9d024b	
+        JA4=t13d191000_9dc949149365_e7c285222651	
+        JA4SSH=c76s76_c71s59_c0s70	
+        JA4T=64240_2-1-3-1-1-4_1460_8	
+        JA4TScan=28960_2-4-8-1-3_1460_3_1-4-8-16	
+    """)
+    print(a.extract(), a.contains())
+    print(b.extract(), b.contains())
