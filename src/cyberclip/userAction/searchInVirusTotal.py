442054cd5a63bd744fcdfbc641728ff7d585187c
SVernin <>
2025-02-04 18:37:39 +0100

    🌱 Add relationsship in Virus Total search actions

--- src/cyberclip/userAction/searchInVirusTotal.py
+++ src/cyberclip/userAction/searchInVirusTotal.py
@@ -12,19 +12,31 @@ import requests
 """A action module, to open search observables contained in a text in Virus Total."""
 
 DEFAULT_PARAMS = {
+                "Relationships":{
+                    "type":"tags",
+                    "value":["contacted_urls","contacted_domains","contacted_ips","dropped_files","bundled_files","itw_urls","submissions"]
+                    },
                 "Analysis fields":{
                     "type":"tags",
-                    "value":["$.data.attributes.last_analysis_stats.malicious","$.data.attributes.tags","$.data.attributes.first_submission_date","$.data.attributes.creation_date","$.data.attributes.reputation","$.data.attributes.meaningful_name","$.data.attributes.magic","$.data.attributes.size","$.data.attributes.jarm","$.data.attributes.asn"]
-                    }
+                    "value":["$.data.attributes.last_analysis_stats.malicious","$.data.attributes.tags","$.data.attributes.first_submission_date","$.data.attributes.creation_date","$.data.attributes.reputation","$.data.attributes.meaningful_name","$.data.attributes.magic","$.data.attributes.size","$.data.attributes.jarm","$.data.attributes.asn","$.data.relationships.bundled_files.data[*].id","$.data.relationships.contacted_domains.data[*].id","$.data.relationships.contacted_ips.data[*].id","$.data.relationships.contacted_urls.data[*].context_attributes.url","$.data.relationships.dropped_files.data[*].id","$.data.relationships.submissions.data[*].attributes.country","$.data.relationships.submissions.data[*].attributes.date"]
+                    },
                 }
 
 class searchInVirusTotalAction(actionInterface):
-    """Search all type of observables with the Virus Total API. The configuration is passed in the config file.
-    
-    A configuration is neeeded : 
+    """Search ip, domain, MD5, SHA1 and SHA256 with the Virus Total API. The configuration should be passed in the config file.
 
-    VirusTotal:
-    - api_key: <VT API Key>
+Two parameters could be passed :
+
+- `Analysis fields` a list of objects to be returned depending of the type of observable searched. JSON scheme is described on [VT](https://docs.virustotal.com/reference/files), you can unset this parameter to retrieve the whole JSON,
+- `Relationships` a list of relationships to query. For each observable results will be added under `$.data.<relationship_name>` key and thus specific fields of relationship could be retrievedwith `$.relationships.relationship_name.<path>` thanks to the `Analysis fields` parameter
+
+
+A configuration is neeeded : 
+
+```yaml
+VirusTotal:
+- api_key: <VT API Key>
+```
     """    
     def __init__(self, parsers ={}, supportedType = {"ip","domain","md5","sha1","sha256"}, complex_param=DEFAULT_PARAMS):
         super().__init__(parsers = parsers, supportedType = supportedType, complex_param=complex_param)
@@ -39,17 +51,19 @@ class searchInVirusTotalAction(actionInterface):
     def execute(self) -> object:
         self.observables = self.get_observables()
         self.results = {}
+        
+        relationships = ",".join(self.get_param_value("Relationships")) if self.get_param_value("Relationships") else ""
         for obs_type in self.supportedType:
             if obs_type=="ip" and (observables:=self.observables.get(obs_type,[])):
                 for ip in observables:
-                    url = f"https://www.virustotal.com/api/v3/ip_addresses/{ip}"
+                    url = f"https://www.virustotal.com/api/v3/ip_addresses/{ip}?relationships={relationships}"
                     try:
                         self.results[ip] = response.json()
                     except:
                         self.results[ip] = {}
             if obs_type=="domain" and (observables:=self.observables.get(obs_type,[])):
                 for domain in observables:
-                    url = f"https://www.virustotal.com/api/v3/domains/{domain}"
+                    url = f"https://www.virustotal.com/api/v3/domains/{domain}?relationships={relationships}"
                     try:
                         response = requests.get(url, headers=self.headers)
                         self.results[domain] = response.json()
@@ -57,7 +71,7 @@ class searchInVirusTotalAction(actionInterface):
                         self.results[domain] = {}                   
             if obs_type in ["md5","sha1","sha256"] and (observables:=self.observables.get(obs_type,[])):
                 for file_hash in observables:
-                    url = f"https://www.virustotal.com/api/v3/files/{file_hash}"
+                    url = f"https://www.virustotal.com/api/v3/files/{file_hash}?relationships={relationships}"
                     try:
                         response = requests.get(url, headers=self.headers)
                         self.results[file_hash] = response.json()
@@ -65,15 +79,19 @@ class searchInVirusTotalAction(actionInterface):
                         self.results[file_hash] = {}
 
         for observable, result in self.results.items():
-            data = {}
-            for selector in self.get_param_value("Analysis fields"):
-                cleaned_selector = selector 
-                if cleaned_selector.startswith("$.data.attributes."):
-                    cleaned_selector = cleaned_selector.replace("$.data.attributes.", "")
-                if seen := jsonpath(result, selector):
-                    data[cleaned_selector] = seen
-                else:
-                    data[cleaned_selector] = ""
+            data = result
+            if self.get_param_value("Analysis fields"):
+                data = {}
+                for selector in self.get_param_value("Analysis fields"):
+                    cleaned_selector = selector 
+                    if cleaned_selector.startswith("$.data.attributes."):
+                        cleaned_selector = cleaned_selector.replace("$.data.attributes.", "")
+                    if cleaned_selector.startswith("$.data.relationships."):
+                        cleaned_selector = cleaned_selector.replace("$.data.relationships.", "")
+                    if seen := jsonpath(result, selector):
+                        data[cleaned_selector] = seen
+                    else:
+                        data[cleaned_selector] = ""
             self.results[observable] = data
         return self.results
     
